4832ca2535d931e2f9848c9ee8aacdbcbb2ee4a9
asif tunga mubarek <asiftunga@hotmail.com>
2023-03-29 06:26:37 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity //obje olusturulamasin - abstract classlar ortak prop ve metotlarin tanimlandigi yerlerdir, interfacelerde sadece sozlesmeler tanimlanir (version 8 e kadar), ayrica interface ile bir farki c# da classlar birden fazla classtan inheritence olamazlar fakat birden fazla interfaceden olabilirler ayrica abstract classlarin ctorlari diger classlari etkileyebilir
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDAte { get; set; }
+    }
+}
