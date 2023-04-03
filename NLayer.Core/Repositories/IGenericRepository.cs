b3a6f3d9398f8671f8aaa00a9fd06162e7f3e52a
asif tunga mubarek <asiftunga@hotmail.com>
2023-04-03 09:58:33 +0300

    core katmani tamamlandi

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 
 namespace NLayer.Core.Repositories
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class //veritabanina yapilacak tum sorgular buraya eklenecek (dbden data alma ile ilgili islemleri gerceklestirir.)
     {
+        Task<T> GetByIdAsync(int id);
+
+        // productRepository.where(x=>x.id>5).orderby.ToListAsync();
+        // alt kismi neden list yapmadik? | bunun nedeni yukaridaki kodu su sekilde calistirirdi -> orderby'dan onceki kisma kadar giderdi sonrasinda ise gelen sonucu memorye alir sonrasinda orderby islemi yapardi, fakat IQueryable seklinde donerse orderby kismini da databasede islem gerceklestirir
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entites);
+
+        void Update(T entity); //efcore update ve delete icin async metotlari yoktur gerek de yoktur
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entites);
     }
 }
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class //GenericRepodan alinana datayi isler ve controllerlara istedigi datayi hazirlamak icindir. Business kod service icerisinde yazilir. try-catch yazacaksak serviceler icerisinde yazilir. DTO donusumleri gerceklestireceksek yine service siniflarinda bu islem yapilir
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entites);
+
+        Task UpdateAsync(T entity);  //veritabanina bu degisiklikler yansitilacagi ve savechangeasync metot kullanilacagi icin asenkrona donusturulur
+
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entites);
+    }
+}
