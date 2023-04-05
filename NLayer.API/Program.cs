63e8b17ecc0f28cbba732a2b6de6660cfa1cd0e3
asif tunga mubarek <asiftunga@hotmail.com>
2023-04-05 07:10:05 +0300

    mapper added

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Extensions;
 using NLayer.Core.Repositories;
+using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +22,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>)); //generic olduklarindan dolayi typeof seklinde eklendiler
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));//mapper eklendi
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
